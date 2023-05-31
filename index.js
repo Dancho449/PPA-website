006bb009b53ba48ef42c09caf43bfca413c2aa07
Dancho449 <125226463+Dancho449@users.noreply.github.com>
2023-05-31 14:08:18 -0700

    finished UI for front page
    Front page finally done

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const clear = document.querySelector(".sidebar-clear");
 
 const head = document.querySelector("header");
 
-const reveal = document.querySelectorAll(".holder")
+const reveal = document.querySelectorAll(".holder");
+
+const icon = document.querySelector(".light-dark");
 
 btn.addEventListener('click', function() {
     container.style.display = "block"
@@ -27,3 +29,12 @@ for(i = 0; i < reveal.length; i++) {
         this.classList.toggle('active')
     })
 }
+
+icon.addEventListener('click', function() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "./images/sun.PNG"
+    }else{
+        icon.src = "./images/moon.PNG"
+    }
+})
